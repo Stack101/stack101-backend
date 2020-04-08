faa99efd8629884d9363b40a2058d195da7c0efd
hyeminnoh <shrmffkfk@naver.com>
2020-04-08 22:55:47 +0900

    [Delete] 파일명 변경을 위해 삭제

--- routes/aggregate.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const aggregateController = require('controllers/aggregate');
-
-router.get('/', aggregateController.getResult);
-
-module.exports = router;
