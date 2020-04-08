6fe9a3661937bc07716a139d57ddaf3b78929d8e
hyeminnoh <shrmffkfk@naver.com>
2020-04-08 22:57:20 +0900

    [Add] aggregate라는 파일명을 statistics로 변경

---
+++ routes/statistics.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const aggregateController = require('controllers/statistics');
+
+router.get('/', aggregateController.getResult);
+
+module.exports = router;
