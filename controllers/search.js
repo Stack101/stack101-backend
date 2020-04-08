b22557101e1fac59e6d873d44d1b2633d8bf1b17
hyeminnoh <shrmffkfk@naver.com>
2020-04-09 01:42:59 +0900

    [Add] search controller 생성, 키워드 조회 구현

---
+++ controllers/search.js
@@ -0,0 +1,18 @@
+const Company = require('models/company');
+const Stack = require('models/stack');
+const catchAsync = require('utils/catchAsync');
+const AppError = require('utils/appError');
+
+exports.getResult = catchAsync(async (req, res, next) => {
+  const { keyword } = req.params;
+
+  // 1. keyword와 일치하는 이름을 가진 데이터 존재 확인 (대소문자 구분없이 검색되도록 정규표현식 활용)
+  // 2. 해당 데이터 뽑아서 응답
+  const company = await Company.findOne({ name: { $regex: keyword, $options: 'i' } });
+  const stack = await Stack.find({ name: { $regex: keyword, $options: 'i' } });
+  if (company || stack) {
+    res.json({ ok: 1, msg: 'Http Result Code 200 OK', item: { companies: company, stacks: stack } });
+  } else {
+    next(new AppError(404, 'ITEM_DOESNT_EXIST'));
+  }
+});
