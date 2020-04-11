6c87f71f95ec0ec1095c072bd74f9930f6fe0931
hyeminnoh <shrmffkfk@naver.com>
2020-04-11 16:48:08 +0900

    [Update] csv 파일 스키마 형태에 맞게 import
    참조관계에 맞는 ObjectId 추가도 완료

--- data/index.js
+++ data/index.js
@@ -1,60 +1,105 @@
-const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const csvtojson = require('csvtojson');
 const Company = require('../models/company');
 const Stack = require('../models/stack');
 
 dotenv.config({ path: '../.env' });
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log('DB connection success');
-  });
-
-const stacks = JSON.parse(fs.readFileSync(`${__dirname}/stacks.json`, 'utf-8'));
-const companies = JSON.parse(fs.readFileSync(`${__dirname}/companies.json`, 'utf-8'));
-
-console.log(stacks);
-console.log(companies);
-
+// 기술스택 정보 추가
 const insertStacks = async () => {
-  await Stack.create(stacks);
-  console.log('inserted stacks dev data');
+  await csvtojson()
+    .fromFile(`${__dirname}/stacks.csv`)
+    .then((stack) => {
+      Stack.create(stack);
+    });
 };
 
+// 참조 ObjectId 추가
+const updateRefId = async (element) => {
+  element.Frontend_Language.split(',').forEach(async (v) => {
+    // 해당 스택의 id 조회
+    const result = await Stack.findOne({ name: v, job_detail: 'Frontend' }, '_id');
+    await Company.findOneAndUpdate({ name: element.Name }, { $push: { stacks: result._id } })
+      .then(async (updatedResult) => {
+        await Stack.findOneAndUpdate({ _id: result._id }, { $push: { companies: updatedResult._id } });
+      });
+  });
+  element.Frontend_Framework.split(',').forEach(async (v) => {
+    const result = await Stack.findOne({ name: v, job_detail: 'Frontend' }, '_id');
+    if (result !== null) {
+      await Company.findOneAndUpdate({ name: element.Name }, { $push: { stacks: result._id } })
+        .then(async (updatedResult) => {
+          await Stack.findOneAndUpdate({ _id: result._id }, { $push: { companies: updatedResult._id } });
+        });
+    }
+  });
+  element.Backend_Language.split(',').forEach(async (v) => {
+    const result = await Stack.findOne({ name: v, job_detail: 'Backend' }, '_id');
+    await Company.findOneAndUpdate({ name: element.Name }, { $push: { stacks: result._id } })
+      .then(async (updatedResult) => {
+        await Stack.findOneAndUpdate({ _id: result._id }, { $push: { companies: updatedResult._id } });
+      });
+  });
+  element.Backend_Framework.split(',').forEach(async (v) => {
+    const result = await Stack.findOne({ name: v, job_detail: 'Backend' }, '_id');
+    await Company.findOneAndUpdate({ name: element.Name }, { $push: { stacks: result._id } })
+      .then(async (updatedResult) => {
+        await Stack.findOneAndUpdate({ _id: result._id }, { $push: { companies: updatedResult._id } });
+      });
+  });
+  element.Android.split(',').forEach(async (v) => {
+    const result = await Stack.findOne({ name: v, job_detail: 'Android' }, '_id');
+    await Company.findOneAndUpdate({ name: element.Name }, { $push: { stacks: result._id } })
+      .then(async (updatedResult) => {
+        await Stack.findOneAndUpdate({ _id: result._id }, { $push: { companies: updatedResult._id } });
+      });
+  });
+  element.IOS.split(',').forEach(async (v) => {
+    const result = await Stack.findOne({ name: v, job_detail: 'IOS' }, '_id');
+    await Company.findOneAndUpdate({ name: element.Name }, { $push: { stacks: result._id } })
+      .then(async (updatedResult) => {
+        await Stack.findOneAndUpdate({ _id: result._id }, { $push: { companies: updatedResult._id } });
+      });
+  });
+  element.DesignTool.split(',').forEach(async (v) => {
+    const result = await Stack.findOne({ name: v, category: 'Tool' }, '_id');
+    await Company.findOneAndUpdate({ name: element.Name }, { $push: { stacks: result._id } })
+      .then(async (updatedResult) => {
+        await Stack.findOneAndUpdate({ _id: result._id }, { $push: { companies: updatedResult._id } });
+      });
+  });
+};
+
+// 회사 정보 추가
 const insertCompanies = async () => {
-  await Company.create(companies);
-  console.log('inserted companies dev data');
+  await csvtojson()
+    .fromFile(`${__dirname}/companies.csv`)
+    .subscribe(async (element) => {
+      await Company.create({
+        name: element.Name,
+        description: element.Description,
+        category: element.Column,
+        logo: element.Logo,
+        link: element.Link,
+      });
+      await updateRefId(element);
+    });
 };
 
 (async () => {
-  await insertStacks();
-  await insertCompanies();
-
-  const originalCompanies = await Company.find();
-  const originalStacks = await Stack.find();
-
-  originalCompanies.forEach(async (e) => {
-    const randNum = Math.ceil(Math.random() * (8 - 6) + 6);
-    const randStacksArr = [];
-    const compStacksArr = [];
-
-    new Array(randNum).fill(0).forEach(async (_) => {
-      const randStackIdx = Math.ceil(Math.random() * originalStacks.length - 1);
-
-      if (!~randStacksArr.indexOf(randStackIdx)) {
-        randStacksArr.push(randStackIdx);
-        const currentStackId = originalStacks[randStackIdx]._id;
-        compStacksArr.push(currentStackId);
-        await Stack.updateOne({ _id: currentStackId }, { $push: { companies: e._id } });
-      }
-    });
-
-    await Company.updateOne({ _id: e._id }, { $set: { stacks: compStacksArr } });
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }).then(() => {
+    console.log('DB connection success');
   });
+  // Stack.companies를 제외하고 스택정보 추가
+  await insertStacks();
+
+  /* Company.stacks를 제외하고 회사정보 추가,
+  참조관계에 맞는 ObjectId값을 companies, stacks에 추가 */
+  await insertCompanies();
 })();
