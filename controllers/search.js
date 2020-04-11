3fc9ab50dad563649d04a90b75ebc32306470b4c
hyeminnoh <shrmffkfk@naver.com>
2020-04-11 17:25:56 +0900

    [Update] populate로 치환되는 객체의 참조 Id들은 반환하지 않도록 수정

--- controllers/search.js
+++ controllers/search.js
@@ -7,11 +7,11 @@ exports.getResult = catchAsync(async (req, res, next) => {
   const { keyword } = req.params;
 
   // 1. keyword와 일치하는 이름을 가진 데이터 존재 확인 (대소문자 구분없이 검색되도록 정규표현식 활용)
-  // 2. 해당 데이터 뽑아서 응답
-  const company = await Company.findOne({ name: { $regex: keyword, $options: 'i' } }).populate('stacks');
-  const stack = await Stack.find({ name: { $regex: keyword, $options: 'i' } }).populate('companies');
+  // 2. 참조 ObjectId를 실제 객체로 반환할 때, 그 객체의 참조 Id까지는 반환할 필요 없으므로 제외해서 조회
+  const company = await Company.find({ name: { $regex: keyword, $options: 'i' } }).populate('stacks', { companies: false });
+  const stack = await Stack.find({ name: { $regex: keyword, $options: 'i' } }).populate('companies', { stacks: false });
   if (company || stack) {
-    res.json({ ok: 1, msg: 'Http Result Code 200 OK', item: { companies: company, stacks: stack } });
+    res.json({ ok: 1, msg: 'Http Result Code 200 OK', item: { companyResult: company, stackResult: stack } });
   } else {
     next(new AppError(404, 'ITEM_DOESNT_EXIST'));
   }
