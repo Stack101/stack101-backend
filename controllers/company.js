87ff9f7c15cd14261f8d6b20d262c882f5e708c1
hyeminnoh <shrmffkfk@naver.com>
2020-04-21 15:34:09 +0900

    [Update] 전체 조회 수정
    limit 추가, 회사명 순으로 정렬

--- controllers/company.js
+++ controllers/company.js
@@ -18,18 +18,45 @@ exports.getAllCompanies = catchAsync(async (req, res) => {
 
   let companies;
   if (limit) {
-    companies = await Company.find(queryObj).sort('-cnt').limit(limit);
+    companies = await Company.aggregate([
+      { $match: queryObj },
+      {
+        $project: {
+          _v: 1,
+          name: 1,
+          description: 1,
+          category: 1,
+          link: 1,
+          logo: 1,
+          cnt: { $cond: { if: { $isArray: '$stacks' }, then: { $size: '$stacks' }, else: 0 } },
+        },
+      },
+      { $limit: limit },
+      { $sort: { name: 1 } },
+    ]);
   } else {
-    companies = await Company.find(queryObj).sort('-cnt');
+    companies = await Company.aggregate([
+      { $match: queryObj },
+      {
+        $project: {
+          _v: 1,
+          name: 1,
+          description: 1,
+          category: 1,
+          link: 1,
+          logo: 1,
+          cnt: { $cond: { if: { $isArray: '$stacks' }, then: { $size: '$stacks' }, else: 0 } },
+        },
+      },
+      { $sort: { name: 1 } },
+    ]);
   }
 
   res.json({ ok: 1, msg: 'Http Result Code 200 OK', item: companies });
 });
 
 exports.getCompany = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-
-  const company = await Company.findOne({ _id: id }).populate('stacks');
+  const company = await Company.findById(req.params.id).populate('stacks', { companies: 0 });
   if (company) {
     res.json({ ok: 1, msg: 'Http Result Code 200 OK', item: company });
   } else {
