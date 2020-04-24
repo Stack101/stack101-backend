929c63dd2f75db7f346200f082824b3dc531be5a
hyeminnoh <shrmffkfk@naver.com>
2020-04-25 02:34:46 +0900

    [Update] virtual field 제거

--- models/company.js
+++ models/company.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 
 const companySchema = new Schema(
   {
+    id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true, collation: { locale: 'en' } },
     description: { type: String, required: true },
     category: { type: String, required: true },
@@ -11,16 +12,11 @@ const companySchema = new Schema(
     logo: { type: String, required: true },
     stacks: [{ type: Schema.Types.ObjectId, ref: 'Stack' }],
   },
-  {
-    toJSON: { virtuals: true },
-  },
 );
 
-companySchema.virtual('cnt').get(() => {
-  if (this.stacks) {
-    return this.stacks.length;
-  }
-  return 0;
+companySchema.virtual('cnt').get(function getCnt() {
+  return (this.stacks && this.stacks.length) || 0;
 });
+
 
 module.exports = mongoose.model('Company', companySchema);
