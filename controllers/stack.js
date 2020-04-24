c613005b91ed99e1f993eb3a2562a2b98a5d99dd
hyeminnoh <shrmffkfk@naver.com>
2020-04-25 02:31:11 +0900

    [Update] 스택 ObjectId 검색 수정
    스택 정보 내부의 회사정보에 실제 객체 치환 시,
    객체의 내용은 id, 이름, 로고 image url, 해당 회사의 스택개수 정보만 담김

--- controllers/stack.js
+++ controllers/stack.js
@@ -1,3 +1,4 @@
+const Mongoose = require('mongoose');
 const Stack = require('models/stack');
 const catchAsync = require('utils/catchAsync');
 const AppError = require('utils/appError');
@@ -59,8 +60,47 @@ exports.getAllStacks = catchAsync(async (req, res) => {
 
 exports.getStack = catchAsync(async (req, res, next) => {
   const { id } = req.params;
+  const { ObjectId } = Mongoose.Types;
 
-  const stack = await Stack.findOne({ _id: id }).populate('companies');
+  const stack = await Stack.aggregate([
+    { $match: { _id: new ObjectId(id) } },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        job_type: 1,
+        job_detail: 1,
+        category: 1,
+        description: 1,
+        logo: 1,
+        companies: 1,
+        cnt: { $cond: { if: { $isArray: '$companies' }, then: { $size: '$companies' }, else: 0 } },
+      },
+    },
+    {
+      $lookup: {
+        from: 'companies',
+        localField: 'companies',
+        foreignField: '_id',
+        as: 'companies',
+      },
+    },
+    {
+      $addFields: {
+        companies: {
+          $map: {
+            input: '$companies',
+            in: {
+              _id: '$$this._id',
+              name: '$$this.name',
+              logo: '$$this.logo',
+              cnt: { $size: '$$this.stacks' },
+            },
+          },
+        },
+      },
+    },
+  ]);
   if (stack) {
     res.json({ ok: 1, msg: 'Http Result Code 200 OK', item: stack });
   } else {
