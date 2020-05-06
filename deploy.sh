71be5fbe705f5b17f65f82e625c37e958070171f
hyeminnoh <shrmffkfk@naver.com>
2020-05-07 01:39:34 +0900

    [Update] deploy script 수정
    환경변수 값을 활용하지 못해서 ec2 내부 스크립트를 실행하게 함

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-pm2 kill
-
-cd /home/ec2-user/app/stack101-backend/zip
-
-echo "> 새 어플리케이션 배포"
-
-npm start
+/home/ec2-user/app/stack101-backend/start.sh > /dev/null 2> /dev/null < /dev/null &
