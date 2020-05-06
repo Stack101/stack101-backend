e505bf0afb9eef321c24fd785e22ab34f236ec86
hyeminnoh <shrmffkfk@naver.com>
2020-05-06 22:22:52 +0900

    [Fix] deploy 스크립트

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-echo "> 새 어플리케이션 배포"
+pm2 stop server
+
+cd /home/ec2-user/app/stack101-backend/zip
+
+npm install
 
 npm start
