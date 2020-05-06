1a31c6c6dbed8a6cb12efd60c9945aedc6bd545e
hyeminnoh <shrmffkfk@naver.com>
2020-05-06 23:35:50 +0900

    [Fix] nohup 명령 제거

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-REPOSITORY=/home/ec2-user/app/stack101-backend/zip
+pm2 kill
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -f stack101-backend)
-
-echo "$CURRENT_PID"
-
-if [ -z $CURRENT_PID ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
+cd /home/ec2-user/app/stack101-backend/zip
 
 echo "> 새 어플리케이션 배포"
 
-cd $REPOSITORY
-
-nohup npm start > /home/ec2-user/app/logs 2>&1 </home/ec2-user/app/errors &
+npm start
